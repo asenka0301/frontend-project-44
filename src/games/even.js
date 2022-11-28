03d98e79b6b52882ddbbfb39edf118b5ba36b7b6
Nastya <asenka0301@gmail.com>
2022-11-28 14:38:42 +0300

    refactor(even): rename function and fix the return value

--- src/games/even.js
+++ src/games/even.js
@@ -1,6 +1,6 @@
 import { gameFlow, generateNum } from '../index.js';
 
-const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
+const checkIsEven = (num) => num % 2 === 0;
 
 const gameTask = 'Answer "yes" if the number is even, otherwise answer "no".';
 
@@ -8,7 +8,7 @@ export default () => {
   const arrayOfQA = [];
   for (let i = 0; i < 3; i += 1) {
     const question = generateNum(100);
-    const answer = isEven(question);
+    const answer = checkIsEven(question) ? 'yes' : 'no';
     arrayOfQA.push([question, answer]);
   }
   gameFlow(gameTask, arrayOfQA);
