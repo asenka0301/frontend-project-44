5f3c8a7d724c54cbeb55f358378c46dc9d74549a
Nastya <asenka0301@gmail.com>
2022-11-29 20:54:09 +0300

    refactor: fix greatest common divisor functionality

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,13 +1,18 @@
 import { gameFlow, generateNum, GAME_ROUNDS } from '../index.js';
 
-const LEFT_MAX_RANDOM_VALUE = 20;
-const RIGHT_MAX_RANDOM_VALUE = 20;
+const MAX_RANDOM_VALUE_NUM1 = 20;
+const MAX_RANDOM_VALUE_NUM2 = 20;
 
-const gameTask = 'Find the greatest common divisor of given numbers';
+const gameTask = 'Find the greatest common divisor of given numbers.';
 
 const findGcd = (a, b) => {
   let number1 = a;
   let number2 = b;
+
+  if (number2 === 0) {
+    return number1;
+  }
+
   while (number1 !== number2) {
     if (number1 > number2) {
       number1 -= number2;
@@ -21,8 +26,8 @@ const findGcd = (a, b) => {
 export default () => {
   const questionsAndAnswersArr = [];
   for (let i = 0; i < GAME_ROUNDS; i += 1) {
-    const number1 = generateNum(LEFT_MAX_RANDOM_VALUE);
-    const number2 = generateNum(RIGHT_MAX_RANDOM_VALUE);
+    const number1 = generateNum(MAX_RANDOM_VALUE_NUM1) + 1;
+    const number2 = generateNum(MAX_RANDOM_VALUE_NUM2);
     const question = `${number1} ${number2}`;
     const answer = `${findGcd(number1, number2)}`;
     questionsAndAnswersArr.push([question, answer]);
