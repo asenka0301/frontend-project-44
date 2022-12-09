24ea7e9d34d99f25ce4bc26348635d78d5d61170
Nastya <asenka0301@gmail.com>
2022-12-09 18:25:21 +0300

    refactor: fix calculator functionality

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,34 +1,35 @@
 import { gameFlow, generateNum, GAME_ROUNDS } from '../index.js';
 
-const LEFT_MAX_RANDOM_VALUE = 10;
-const RIGHT_MAX_RANDOM_VALUE = 10;
+const MAX_RANDOM_VALUE = 10;
 
 const generateOperator = () => {
   const operators = ['+', '-', '*'];
   return operators[generateNum(operators.length)];
 };
-const getResult = (expression) => {
-  const arr = expression.split(' ');
-  switch (arr[1]) {
+
+const getCalculationResult = (num1, num2, sign) => {
+  switch (sign) {
     case '+':
-      return Number(arr[0]) + Number(arr[2]);
+      return num1 + num2;
     case '-':
-      return Number(arr[0]) - Number(arr[2]);
+      return num1 - num2;
     case '*':
-      return Number(arr[0]) * Number(arr[2]);
+      return num1 * num2;
     default:
       return 'Error: incorrect operation';
   }
 };
-const generateExpression = (maxLhs, maxRhs) => `${generateNum(maxLhs)} ${generateOperator()} ${generateNum(maxRhs)}`;
 
 const gameTask = 'What is the result of the expression?';
 
 export default () => {
   const questionsAndAnswersArr = [];
   for (let i = 0; i < GAME_ROUNDS; i += 1) {
-    const question = generateExpression(LEFT_MAX_RANDOM_VALUE, RIGHT_MAX_RANDOM_VALUE);
-    const answer = String(getResult(question));
+    const operand1 = generateNum(MAX_RANDOM_VALUE);
+    const operand2 = generateNum(MAX_RANDOM_VALUE);
+    const operator = generateOperator();
+    const question = `${operand1} ${operator} ${operand2}`;
+    const answer = String(getCalculationResult(operand1, operand2, operator));
     questionsAndAnswersArr.push([question, answer]);
   }
   gameFlow(gameTask, questionsAndAnswersArr);
