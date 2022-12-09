b63c22f96b1481fbb73f7ff8f583fe9b85abb717
Nastya <asenka0301@gmail.com>
2022-12-09 18:32:26 +0300

    refactor: fix generate progression functionality

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,13 +6,11 @@ const PROGRESSION_LENGTH = 10;
 const PROGRESSION_MAX_RANDON_VALUE = 20;
 const STEP_MAX_RANDON_VALUE = 7;
 
-const generateProgression = (progressioStart, progressionStep) => {
+const generateProgression = (firstElement, step) => {
   const numbers = [];
-  let start = 1 + generateNum(progressioStart);
-  const step = 1 + generateNum(progressionStep);
   for (let i = 0; i < PROGRESSION_LENGTH; i += 1) {
-    numbers.push(start + step);
-    start += step;
+    const current = firstElement + (i * step);
+    numbers.push(current);
   }
   return numbers;
 };
@@ -20,7 +18,9 @@ const generateProgression = (progressioStart, progressionStep) => {
 export default () => {
   const questionsAndAnswersArr = [];
   for (let i = 0; i < GAME_ROUNDS; i += 1) {
-    const progression = generateProgression(PROGRESSION_MAX_RANDON_VALUE, STEP_MAX_RANDON_VALUE);
+    const firstElement = generateNum(PROGRESSION_MAX_RANDON_VALUE);
+    const step = generateNum(STEP_MAX_RANDON_VALUE);
+    const progression = generateProgression(firstElement, step);
     const index = generateNum(progression.length);
     const answer = String(progression[index]);
     progression[index] = '..';
